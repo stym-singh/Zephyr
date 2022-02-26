6b10d96be185f0d8239d6fa0661186ec5b5af1eb
AG <ayushgaba71@gmail.com>
2022-02-27 02:07:36 +0530

    auth init

---
+++ backend/Authentication/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import register, login, logout
+urlpatterns = [
+    path('register/' , register),
+    path('login/' , login),
+    path('logout/' , logout),
+]
--- backend/Authentication/views.py
+++ backend/Authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def register(request):
+    return HttpResponse("Hello world")
+def login(request):
+    pass
+def logout(request):
+    pass
